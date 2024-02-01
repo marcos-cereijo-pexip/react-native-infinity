aa7ee9d4a552120ad78c6b958bc864cc37b03d32
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-02-01 14:59:06 +0100

    chore: set the mediaStrem optional

--- src/components/RemoteVideo.tsx
+++ src/components/RemoteVideo.tsx
@@ -3,11 +3,11 @@ import type { StyleProp, ViewStyle } from 'react-native'
 import { MediaStream, RTCView } from 'react-native-webrtc'
 
 interface RemoteVideoProps {
-  mediaStream: MediaStream
+  mediaStream: MediaStream | null
   objectFit?: 'contain' | 'cover' | undefined
   style?: StyleProp<ViewStyle>
 }
 
 export const RemoteVideo = (props: RemoteVideoProps): JSX.Element => {
-  return <RTCView streamURL={props.mediaStream.toURL()} objectFit={props.objectFit} style={props.style} />
+  return <RTCView streamURL={props.mediaStream?.toURL()} objectFit={props.objectFit} style={props.style} />
 }
--- src/components/SelfView.tsx
+++ src/components/SelfView.tsx
@@ -3,11 +3,13 @@ import type { StyleProp, ViewStyle } from 'react-native'
 import { MediaStream, RTCView } from 'react-native-webrtc'
 
 interface RemoteVideoProps {
-  mediaStream: MediaStream
+  mediaStream: MediaStream | null
   objectFit?: 'contain' | 'cover' | undefined
   style?: StyleProp<ViewStyle>
 }
 
 export const SelfView = (props: RemoteVideoProps): JSX.Element => {
-  return <RTCView mirror={true} streamURL={props.mediaStream.toURL()} objectFit={props.objectFit} style={props.style} />
+  return (
+    <RTCView mirror={true} streamURL={props.mediaStream?.toURL()} objectFit={props.objectFit} style={props.style} />
+  )
 }
